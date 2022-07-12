f50468a1b05f9b32340a2a5e04dd726a461ef121
Bruno Sales <me@baliestri.dev>
2022-07-12 15:47:32 -0300

    feat: use JwtTokenProvider on AuthService

--- source/Application/Services/Auth/AuthService.cs
+++ source/Application/Services/Auth/AuthService.cs
@@ -1,15 +1,28 @@
 // Copyright (c) Bruno Sales <me@baliestri.dev>. Licensed under the MIT License.
 // See the LICENSE file in the repository root for full license text.
 
+using BuberDinner.Application.Interfaces.Providers;
 using BuberDinner.Application.Interfaces.Services.Auth;
 using BuberDinner.Application.Results.Auth;
 
 namespace BuberDinner.Application.Services.Auth;
 
 public class AuthService : IAuthService {
-  public SuccessfulAuthResult Create(string firstName, string lastName, string email, string password)
-    => new(Guid.NewGuid(), firstName, lastName, email, "token");
+  private readonly IJwtTokenProvider _jwtToken;
 
-  public SuccessfulAuthResult SignIn(string email, string password)
-    => new(Guid.NewGuid(), "firstName", "lastName", email, "token");
+  public AuthService(IJwtTokenProvider jwtToken) => _jwtToken = jwtToken;
+
+  public SuccessfulAuthResult Create(string firstName, string lastName, string email, string password) {
+    Guid userId = Guid.NewGuid();
+    string token = _jwtToken.Generate(userId, firstName, lastName);
+
+    return new SuccessfulAuthResult(userId, firstName, lastName, email, token);
+  }
+
+  public SuccessfulAuthResult SignIn(string email, string password) {
+    Guid userId = Guid.NewGuid();
+    string token = _jwtToken.Generate(userId, "firstName", "lastName");
+
+    return new SuccessfulAuthResult(userId, "firstName", "lastName", email, token);
+  }
 }
