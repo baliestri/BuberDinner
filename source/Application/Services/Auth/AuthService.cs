f7e661ece63eca5eb3e931e784eab0e5264f815b
Bruno Sales <me@baliestri.dev>
2022-07-13 19:22:33 -0300

    feat: implements Create and SignIn with UserRepository

--- source/Application/Services/Auth/AuthService.cs
+++ source/Application/Services/Auth/AuthService.cs
@@ -2,27 +2,45 @@
 // See the LICENSE file in the repository root for full license text.
 
 using BuberDinner.Application.Interfaces.Providers;
+using BuberDinner.Application.Interfaces.Repositories;
 using BuberDinner.Application.Interfaces.Services.Auth;
 using BuberDinner.Application.Results.Auth;
+using BuberDinner.Core.Entities;
 
 namespace BuberDinner.Application.Services.Auth;
 
 public class AuthService : IAuthService {
   private readonly IJwtTokenProvider _jwtToken;
+  private readonly IUserRepository _userRepository;
 
-  public AuthService(IJwtTokenProvider jwtToken) => _jwtToken = jwtToken;
+  public AuthService(IJwtTokenProvider jwtToken, IUserRepository userRepository) {
+    _jwtToken = jwtToken;
+    _userRepository = userRepository;
+  }
 
   public SuccessfulAuthResult Create(string firstName, string lastName, string email, string password) {
-    Guid userId = Guid.NewGuid();
-    string token = _jwtToken.Generate(userId, firstName, lastName);
+    if (_userRepository.GetUserByEmail(email) is not null) {
+      throw new Exception("An user with the provided email already exists");
+    }
 
-    return new SuccessfulAuthResult(userId, firstName, lastName, email, token);
+    User user = new() { Email = email, FirstName = firstName, LastName = lastName, Password = password };
+    _userRepository.Add(user);
+    string token = _jwtToken.Generate(user.Id, firstName, lastName);
+
+    return new SuccessfulAuthResult(user.Id, firstName, lastName, email, token);
   }
 
   public SuccessfulAuthResult SignIn(string email, string password) {
-    Guid userId = Guid.NewGuid();
-    string token = _jwtToken.Generate(userId, "firstName", "lastName");
+    if (_userRepository.GetUserByEmail(email) is not User user) {
+      throw new Exception("An user with the provided email was not found");
+    }
 
-    return new SuccessfulAuthResult(userId, "firstName", "lastName", email, token);
+    if (user.Password != password) {
+      throw new Exception("The provided password does not match");
+    }
+
+    string token = _jwtToken.Generate(user.Id, user.FirstName, user.LastName);
+
+    return new SuccessfulAuthResult(user.Id, user.FirstName, user.LastName, email, token);
   }
 }
